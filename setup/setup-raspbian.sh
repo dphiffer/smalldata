e065833a8502c01cd51ee0958b5b1b2249cf6961
Dan Phiffer <dan@phiffer.org>
2016-05-14 17:54:31 -0400

    🐯 Added message of the day

--- setup/setup-raspbian.sh
+++ setup/setup-raspbian.sh
@@ -6,11 +6,20 @@ if [ -z "$1" ] ; then
 fi
 
 host="$1"
-echo "Setting up host $1"
+echo "Setting up host $host"
 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y screen git ufw fail2ban unattended-upgrades autossh
 
-sudo echo $1 > /etc/hostname
-sudo sed -i "s/raspberrypi/$1/g" /etc/hosts
+sudo echo $host > /etc/hostname
+sudo sed -i "s/raspberrypi/$host/g" /etc/hosts
+
+echo <<< EOF
+   ____           ____  ___       __
+  / __/_ _  ___ _/ / / / _ \___ _/ /____ _
+ _\ \/  ' \/ _ `/ / / / // / _ `/ __/ _ `/
+/___/_/_/_/\_,_/_/_/ /____/\_,_/\__/\_,_/
+
+Meow! Welcome to ${host}.smalldata.coop.
+EOF > /etc/motd
